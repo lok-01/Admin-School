a916e37224f45769a4c069a046e013d55c65a9ab
lok-01 <b.a.lokesh2@gmail.com>
2026-03-11 12:51:51 +0530

    update 1

--- SchoolManagement/obj/Debug/net10.0/SchoolManagement.AssemblyInfo.cs
+++ SchoolManagement/obj/Debug/net10.0/SchoolManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4759250e56e54e449ad43e0b68447ff0d15391")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
